47ff619d624aa11c3c5aaeb25ec13c504d6ad8bd
SeamusOMC <seamusgrew12@outlook.com>
2025-09-22 00:08:59 +0100

    Initial Commit

---
+++ src/test/java/com/example/palindrome_check/PalindromeCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.palindrome_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalindromeCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
